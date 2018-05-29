6759be5c5ed57c9e6c3236c96e0910ae58c8dd97
wang <562859201@qq.com>
2018-05-29 14:23:56 +0800

    2

--- ja/index.php
+++ ja/index.php
@@ -2,20 +2,34 @@
 <html lang="zh-CN">
 <head>
     <meta charset="utf-8">
+    <meta name=”viewport” content=”width=device-width, initial-scale=1″ />
 </head>
+<style>
+    .jumbotron{
+        position: fixed;
+        top: 0px;
+        left: 0px;
+        right: 0px;
+        bottom: 0px;
+        margin: auto;
+        width: 60%;padding-top: 300px;
+    }
+</style>
 <body class="gray-bg">
 <div class="site-index">
+    <div class="jumbotron">
+        <div style="margin-bottom: 8px;"><button id="tip" onclick="tip();">提示</button>&nbsp;&nbsp;&nbsp;&nbsp<button id="refresh" onclick="toRefresh();">刷新</button></div>
 
-    <div class="jumbotron" style="margin: 0 auto;width: 300px;padding-top: 300px;">
-        <table border="1px;" style="font-size: 20px;width: 100%">
+        <table border="1px;" style="font-size: 26px;width: 100%">
             <?php
             require './data.php';
             shuffle($dataProvider);
+
             foreach ($dataProvider as $k=>$d){
                 if($k%5==0){
                     echo "<tr>";
                 }
-                echo "<td>".$d."</td>";
+                echo "<td>".$d[0]."&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span class='tip' style='display: none'>".$d[1]."</span></td>";
                 if(($k+1)%5==0){
                     echo "</tr>";
                 }
@@ -24,5 +38,18 @@
         </table>
     </div>
 </div>
+<script src="/html5/js/jquery.js"></script>
+<script>
+function tip() {
+    $(".tip").fadeIn(2);
+    var  n = 5;
+    timer = setTimeout(function () {
+            $(".tip").fadeOut(2);
+    },'3000');
+}
+function toRefresh() {
+    window.location.href = window.location.href;
+}
+</script>
 </body>
 </html>
